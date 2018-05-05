3165c3b1c0ea5204c5f8bb9c6424bb922120c0e2
Yeeyu <410421213@gms.ndhu.edu.tw>
2018-05-05 16:47:00 +0800

    training

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -8,3 +8,28 @@ imgI=imageio.imread('I.png')
 imgE=imageio.imread('E.png')
 Eprime=imageio.imread('Eprime.png')
 
+epoch=1			 #Set Epoch=1
+weight=np.random.rand(3) #隨機生成初始值
+a=0.00001 		 #LearningRate
+max=10			 #MaxIterLimit
+
+while (epoch==1 or epoch<max):
+	for h in range(300):		#設定範圍
+		for w in range(400):
+			ak=weight[0]*key1[h][w]+weight[1]*key2[h][w]+weight[2]*imgI[h][w]
+			ek=imgE[h][w]-ak
+			weight[0]+=a*ek*key1[h][w]
+			weight[1]+=a*ek*key2[h][w]
+			weight[2]+=a*ek*imgI[h][w]
+	epoch+=1
+	if epoch>=0:
+		print("Output w:",weight)
+		ans=(Eprime-(weight[0]*key1)-(weight[1]*key2))/weight[2]
+		plt.imshow(ans,cmap='gray')
+		plt.savefig('ans.png')
+		plt.show()
+		break
+
+
+
+
